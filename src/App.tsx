5dac49de84778ca168257161fb35ce49c658d7b1
zkauansantos <kauansantosdepontes@gmail.com>
2023-01-24 10:19:37 -0300

    criated header and globalStyles

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,11 @@
+import Header from './components/Header';
+import { GlobalStyles } from './styles/GlobalStyles';
+
 export default function App () {
-	return <h1> Hello Word </h1>;
+	return (
+		<>
+			<GlobalStyles  />
+			<Header />
+		</>
+	);
 }
